bd5b34269dd74ce6dc7acb12cb94cf9a435e8d3b
Jean-Christophe SAAD-DUPUY <jc.saaddupuy@fsfe.org>
2015-06-30 18:37:44 +0200

    yapf

--- lolcommitss/apps/default/views.py
+++ lolcommitss/apps/default/views.py
@@ -19,20 +19,17 @@ from .models import Photo
 blueprint = Blueprint('default', __name__)
 
 
-
-@blueprint.route('/upload', methods=['POST', 'GET'])
-@use_args(
-        {'file': Arg(required=True,
-            validate=lambda f : isinstance(f, FileStorage)),
-     'repo': Arg(str,
-                 required=True), },
-    locations=('files', 'form'))
+@blueprint.route('/upload', methods=['POST', ])
+@use_args({
+    'file': Arg(required=True, validate=lambda f : isinstance(f, FileStorage)),
+    'repo': Arg(str, required=True),
+}, locations=('files', 'form'))  # yapf: disable
 def upload(args):
     repo = args['repo']
     _file = args['file']
-    filename = c.photos.save(_file, folder = repo)
+    filename = c.photos.save(_file, folder=repo)
 
-    photo = Photo(repo = repo, filename = filename)
+    photo = Photo(repo=repo, filename=filename)
     c.db.session.add(photo)
     c.db.session.commit()
 
