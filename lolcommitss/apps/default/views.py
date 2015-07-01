696244dcad4e51d38139623b058ac7589e919eac
Jean-Christophe SAAD-DUPUY <jc.saaddupuy@fsfe.org>
2015-07-01 11:41:08 +0200

    feat: list of repo on root url

--- lolcommitss/apps/default/views.py
+++ lolcommitss/apps/default/views.py
@@ -17,6 +17,11 @@ from flask import url_for
 from flask import render_template
 from .models import Photo
 
+try:
+    from urlib.parse import quote, unquote
+except ImportError:
+    from urllib2 import quote, unquote
+
 blueprint = Blueprint('default', __name__, template_folder='templates')
 
 
@@ -28,6 +33,7 @@ blueprint = Blueprint('default', __name__, template_folder='templates')
 def upload(args):
     repo = args['repo']
     _file = args['file']
+
     filename = c.photos.save(_file, folder=repo)
 
     photo = Photo(repo=repo, filename=filename)
@@ -38,9 +44,15 @@ def upload(args):
 
 @blueprint.route('/', methods=['GET', ])
 def root():
-    return render_template('root.html.j2')
 
-@blueprint.route('/repo/<repo>', methods=['GET', ])
+    from sqlalchemy.sql import select
+    stmt = select([Photo.repo]).group_by(Photo.repo)
+    results = c.db.session.execute(stmt)
+
+    results = {r[0] : '/repo/'+quote(r[0],safe='') for r in results}
+    return render_template('root.html.j2', repos = results)
+
+@blueprint.route('/repo/<path:repo>', methods=['GET', ])
 def repo(repo):
     photos = c.db.session.query(Photo).filter(Photo.repo == repo).all()
     return render_template('repo.html.j2', photos = photos)
